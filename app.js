9119d1f311f41cb96cf40a9deef53b9bc80afb44
arsenicum32 <zombiehot@yandex.ru>
2016-09-06 13:28:56 +0300

    go

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ var bot = new TelegramBot(token, {polling: true});
 bot.on('message', function (msg) {
     var chatId = msg.chat.id;
     console.log(msg);
-    request('http://www.google.com', function (error, response, body) {
+    request('https://raw.githubusercontent.com/arsenicum32/ars32bot/master/mes.json', function (error, response, body) {
       if (!error && response.statusCode == 200) {
         var ob = JSON.parse(body);
-        if(ob[msg]){
-          bot.sendMessage(chatId, ob[msg] , {caption: "I'm a bot!"});
-        }
+        console.log(ob.msg);
+        ob.msg?
+          bot.sendMessage(chatId, ob.msg , {caption: "I'm a bot!"}) : void(0);
       }
     });
 });
