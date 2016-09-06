15e1c242b24a9c5a1f065b49ac1fae2c94ff8532
arsenicum32 <zombiehot@yandex.ru>
2016-09-06 13:23:51 +0300

    go

---
+++ app.js
@@ -0,0 +1,20 @@
+//191405159:AAGS1kw2GtE3VnhG_8y3UBYpd1zP_pr-cvI
+
+var TelegramBot = require('node-telegram-bot-api');
+var request = require('request');
+var token = '191405159:AAGS1kw2GtE3VnhG_8y3UBYpd1zP_pr-cvI';
+var bot = new TelegramBot(token, {polling: true});
+
+
+bot.on('message', function (msg) {
+    var chatId = msg.chat.id;
+    console.log(msg);
+    request('http://www.google.com', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var ob = JSON.parse(body);
+        if(ob[msg]){
+          bot.sendMessage(chatId, ob[msg] , {caption: "I'm a bot!"});
+        }
+      }
+    });
+});
